f60edb79e9505cf0957e236cf68bbbf504d16e8f
Mervan Kaya <mckaya@kth.se>
2020-12-16 17:15:29 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,3 @@
+print("Hej") #Skriver Hej
+name = input("Vad heter du?") #Skriver ut parametern, väntar på en inmatning av användaren
+print("Ohhhh", name, "Vilket fint namn") #Skriver ut parameter, inputen som användaren gav och sedan nästan nästa parameter
